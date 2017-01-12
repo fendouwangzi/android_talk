a375578e8269913a9946b8d1ec0cf2195ceb62c1
zg <408323641@qq.com>
2017-01-12 15:35:15 +0800

    添加日志

--- app/src/main/java/sanocare/com/talk/AdaptationActivity.java
+++ app/src/main/java/sanocare/com/talk/AdaptationActivity.java
@@ -19,6 +19,7 @@ public class AdaptationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adaptation);
+        Log.e(TAG, TAG + "----start onCreate~~~");
     }
 
     @Override
